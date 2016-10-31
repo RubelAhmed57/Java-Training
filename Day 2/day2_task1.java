f3aeef103d06c3c819f5a89c2573b98b8594a6d0
Rubel Ahmed <rubel9ahmed@gmail.com>
2016-11-01 00:40:03 +0600

    Add files via upload

---
+++ Day 2/day2_task1.java
@@ -0,0 +1,27 @@
+public class day2_task1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       char ch1 = 'X';
+       char ch2 = 86;  //ascii value is obtained from int value
+       System.out.println(ch1);
+       System.out.println(ch2);
+       ch1 ++; // ascii value conterved to int value and increased
+       ch2 --; // ascii value conterved to int value and decreased
+       System.out.println(ch1);
+       System.out.println(ch1);
+    
+}
+}
+
+
+*/what will be the output of the program?
+ans: 
+X
+V
+Y
+Y
+
+/*
