eec92151c0a4425a7505a3d896d599cc1a1b030a
Rubel Ahmed <rubel9ahmed@gmail.com>
2016-11-01 01:14:35 +0600

    Create day2_task2.java

---
+++ Day 2/day2_task2.java
@@ -0,0 +1,37 @@
+public class day2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        boolean b1 = true;
+        System.out.println(b1);
+        if(b1)
+        {
+               b1 = false;
+               System.out.println(b1);
+               
+        }
+        else 
+        {
+                  System.out.println("Not ok");
+        }
+        if(b1)
+        {
+               System.out.println("Sommossa hai...:)");
+        }
+        else
+        {
+               System.out.println("Problem nahi hai...:/");
+        }
+    
+}
+}
+*/
+output: 
+true
+false
+Problem nahi hai...:/
+try to explain urself.
+/*
